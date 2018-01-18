eec0b65ff732437ac09decc3698da3e136c64e4d
Hirsh Agarwal <hirsh.agarwal@gmail.com>
2018-01-18 15:01:13 +0000

    Image displays and grid is drawn on top.

--- TheiaJava/src/com/hirshagarwal/theia/Display.java
+++ TheiaJava/src/com/hirshagarwal/theia/Display.java
@@ -1,26 +1,28 @@
 package com.hirshagarwal.theia;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 public class Display {
 	
 	private JFrame frame;
-	private final int width = 700;
+	private final int width = 900;
 	private final int height = 700;
 	private final JFileChooser fc = new JFileChooser();
 	
@@ -30,6 +32,7 @@ public class Display {
 		frame.setSize(width, height);
 		Container contentPane = frame.getContentPane();
 		contentPane.setLayout(new FlowLayout());
+
 		// Create the display elements
 		JButton selectImageButton = new JButton("Select Image");
 		selectImageButton.setBounds(0, 0, 120, 40);
@@ -47,19 +50,46 @@ public class Display {
 		selectImageButton.addActionListener(selectImageAction);
 		frame.add(selectImageButton);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//		frame.setLayout(null);
 		frame.setVisible(true);
 	}
 	
+	
+	/***
+	 * Draws an image with a grid on top so that the user can select crops from it.
+	 * @param image
+	 */
 	public void drawImage(BufferedImage image){
-		// TODO: Make images scale correctly
-		int imageLabelWidth = 1392/2;
-		int imageLabelHeight = 1040/2;
-		Image scaledImage = image.getScaledInstance(imageLabelWidth, imageLabelHeight, Image.SCALE_SMOOTH);
-		JLabel imageLabel = new JLabel(new ImageIcon(scaledImage));
-		imageLabel.setBounds(0, 50, imageLabelWidth, imageLabelHeight);
-		imageLabel.setVisible(true);
-		frame.add(imageLabel);
-		frame.repaint();
+		Dimension imagePaneSize = new Dimension(696, 520);
+		int gridSize = 10;
+		
+		// Create image to draw grid lines on
+		BufferedImage gridImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2d = gridImage.createGraphics();
+		g2d.drawImage(image, 0, 0, null);
+		g2d.setBackground(Color.WHITE);
+		BasicStroke bs = new BasicStroke(2);
+		g2d.setStroke(bs);
+		
+		// Draw white grid lines
+		for(int i=0; i<gridSize+1; i++){
+			g2d.drawLine((image.getWidth()+2)/gridSize*i, 0, (image.getWidth()+2)/gridSize*i, image.getHeight()-1);
+			g2d.drawLine(0, (image.getHeight()+2)/gridSize*i, image.getWidth()-1, (image.getHeight()+2)/gridSize*i);
+		}
+		
+		// Resize Image
+		BufferedImage resizedImage = new BufferedImage((int)imagePaneSize.getWidth(), (int)imagePaneSize.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		Image tmp = gridImage.getScaledInstance((int)imagePaneSize.getWidth(), (int)imagePaneSize.getHeight(), Image.SCALE_SMOOTH);
+		JPanel imagePane = new JPanel(){
+			@Override
+			protected void paintComponent(Graphics g){
+				super.paintComponent(g);
+				g.drawImage(tmp, 0, 0, null);
+			}
+		};
+		imagePane.setPreferredSize(imagePaneSize);
+		frame.add(imagePane);
+		frame.setVisible(true);
 	}
-
+	
 }
