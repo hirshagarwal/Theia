9a4bc9a19347625c31d88c49540afb6c8c602d59
Hirsh Agarwal <hirsh.agarwal@gmail.com>
2018-02-15 12:28:43 +0000

    Working on system to threshold images

--- TheiaJava/src/com/hirshagarwal/theia/Main/ThresholdImages.java
+++ TheiaJava/src/com/hirshagarwal/theia/Main/ThresholdImages.java
@@ -1,5 +1,49 @@
 package com.hirshagarwal.theia.Main;
 
-public class ThresholdImages {
+import java.awt.image.BufferedImage;
 
+public class ThresholdImages {
+	
+	private BufferedImage rawImage;
+	private BufferedImage thresholdImage;
+	
+	
+	/**
+	 * Constructor to take an input image and threshold and use them to initialize the object
+	 * @param inputImage
+	 * @param threshold
+	 */
+	public ThresholdImages(BufferedImage inputImage, int threshold) {
+		rawImage = inputImage;
+		
+	}
+
+	/**
+	 * Method to take an input image and a threshold value and return the thresholded image
+	 * @param inputImage
+	 * @param threshold
+	 * @return
+	 */
+	public BufferedImage thresholdImage(int threshold) {
+		//TODO: Fill out method
+		return null;
+	}
+
+	
+	/**
+	 * Get the original image
+	 * @return
+	 */
+	public BufferedImage getRawImage() {
+		return rawImage;
+	}
+	
+	/**
+	 * Get the thresholded image
+	 * @return
+	 */
+	public BufferedImage getImage() {
+		return thresholdImage;
+	}
+	
 }
